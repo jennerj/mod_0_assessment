c2085d5c3f399a806086cb88f14a4863775c7ee8
Jacob Jenner <jennerjacob@gmail.com>
2021-07-30 16:06:13 -0600

    Add model_a.rb

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+laptop_musts = {
+  "brand" => 'MSI',
+  "keyboard" => 'Full',
+  "min_ideal_weight" => 2, #lbs
+  "max_ideal_weight" => 5.5, #lbs
+  "has_numpad" => true,
+  "has_webcam" => true,
+  "ideal_colors" => ['Black', 'Red', 'Grey'],
+  "ideal_size" => ['19 inches', '20 inches', '21 inches']
+  }
