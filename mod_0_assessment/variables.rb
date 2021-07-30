23d76ad49ce251f6d68a7cfeba929b7af1f7a50c
Jacob Jenner <jennerjacob@gmail.com>
2021-07-30 15:51:54 -0600

    Complete program specific challenge

---
+++ mod_0_assessment/variables.rb
@@ -0,0 +1,11 @@
+animal = "Dog"
+is_cuddly = true
+animal = "Cat"
+num_8 = 8
+num_3 = 3
+quotient = num_3 / num_8
+favorite_films = "Grandma's boy, The Two Towers LOTR, Star Wars"
+
+puts favorite_films.shift
+
+puts animal.length
